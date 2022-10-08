3981d6b241c8bd426bc15e6b009da563f0410cc0
FedynaCarlos <jfedyna@outlook.com>
2022-10-08 16:18:19 -0300

    Get

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -7,6 +7,28 @@ const controller = {
 	},
 	home: (req, res) => {
 		return res.render('home');
+	},
+	list: function(req, res) {
+		//en este lugar puedo iterar o ir a buscar datos a algun ladola clave esta en
+		//usar el mismo nombre, que el segundo parametro del render
+		//let "users" y el segundo parametro del render seria igual {"users": users}
+		let users = [ 'Dario', 'Carlos', 'Juan', 'Paco', 'Ale' ];
+		res.render('userList', { users: users });
+		//-------------------------!users:  ese nombre debe coincidir con el que usaremos en la vista)
+	},
+
+	comida: (req, res) => {
+		return res.render('indexx');
+	},
+	casa: (req, res) => {
+		let users1 = [
+			{ id: 1, name: 'Dario' },
+			{ id: 2, name: 'Juan' },
+			{ id: 3, name: 'Carlos' },
+			{ id: 4, name: 'Maru' },
+			{ id: 5, name: 'Ale' }
+		];
+		res.render('userList1', { users1: users1 });
 	}
 };
 
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -6,4 +6,8 @@ const mainController = require('../controllers/mainController');
 router.get('/', mainController.index);
 router.get('/about', mainController.about);
 router.get('/home', mainController.home);
+router.get('/list', mainController.list);
+router.get('/comida', mainController.comida);
+router.get('/casa', mainController.casa);
+
 module.exports = router;
